e42e9f92ad86e53ce7079f6fe3af152e2b8ac309
GopiAshreeJay <GopiAshreeJay@192.168.1.16>
2019-04-15 16:01:01 +0530

    2nd commit

--- src/testPackageA/demo2.java
+++ src/testPackageA/demo2.java
@@ -1,15 +1,15 @@
-package testPackage;
+package testPackageA;
 
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class demoClass 
+public class demo2 
 {
 @Test
 	
 	public void gopi()
 	{
-		Reporter.log("hiiii",true);
+		Reporter.log("bye",true);
 		Reporter.log("hello",true);
 	}
 
