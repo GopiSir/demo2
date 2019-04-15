f67f3965ebb8b0ec5e03374f4751a7f2b4f5d0aa
GopiAshreeJay <GopiAshreeJay@192.168.1.16>
2019-04-15 15:36:58 +0530

    1st project

---
+++ src/testPackage/demoClass.java
@@ -0,0 +1,15 @@
+package testPackage;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demoClass 
+{
+@Test
+	
+	public void gopi()
+	{
+		Reporter.log("hiiii",true);
+	}
+
+}
